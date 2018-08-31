3dcb744ac75bd3038469f6b05f117c975a8d26d1
Benedikt Schmidt <benediktibk@outlook.com>
2018-08-31 21:11:48 +0200

    Add Dummy-API-Project

---
+++ MilleniumFalconServer/Api/Controllers/MonitoringController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("monitoring")]
+    public class MonitoringController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Läuft!";
+        }
+    }
+}
