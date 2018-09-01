e8cb38358818346c88d5e9eaa146554eb11bc37c
Benedikt Schmidt <benediktibk@outlook.com>
2018-09-01 10:42:55 +0200

    Set process name of service with custom function

--- MilleniumFalconClient/falcon-service.py
+++ MilleniumFalconClient/falcon-service.py
@@ -3,7 +3,7 @@
 import signal
 import time
 import logging
-import procname
+from ctypes import cdll, byref, create_string_buffer
 
 logger = logging.getLogger()
 handler = logging.FileHandler("/var/log/falcon-service")
@@ -12,25 +12,32 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
+def setprocessname(processname):	
+	libc = cdll.LoadLibrary('libc.so.6')
+	buff = create_string_buffer(len(processname) + 1)
+	buff.value = processname
+	libc.prctl(15, byref(buff), 0, 0, 0)
+
 logger.debug("set process name")
-procname.setprocname("falcon-service")
+setprocessname("falcon-service")
 
 class GracefulKiller:
-  kill_now = False
-  def __init__(self):
-    signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
+	kill_now = False
+	def __init__(self):
+		signal.signal(signal.SIGINT, self.exit_gracefully)
+		signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-  def exit_gracefully(self, signum, frame):
-    logger.info("received signal to stop")
-    self.kill_now = True
+	def exit_gracefully(self, signum, frame):
+		logger.info("received signal to stop")
+		self.kill_now = True
 
 if __name__ == '__main__':
-  killer = GracefulKiller()
-  while True:
-    time.sleep(1)
-    logger.info("doing something important")
-    if killer.kill_now:
-      break
+	killer = GracefulKiller()
+	
+	while True:
+		time.sleep(1)
+		logger.info("doing something important")
+		if killer.kill_now:
+			break
 
 logger.info("stopping gracefully")
