54cb449850575dafbf0eb11de2a888e7462fd425
Benedikt Schmidt <benediktibk@outlook.com>
2018-09-01 10:10:15 +0200

    Add falcon-service

--- MilleniumFalconClient/falcon-update.py
+++ MilleniumFalconClient/falcon-update.py
@@ -5,6 +5,8 @@ from shutil import copyfile
 
 sourcePath = "/usr/src/milleniumfalcon"
 falconUpdate = "/usr/bin/falcon-update.py"
+falconService = "/usr/bin/falcon-service.py"
+falconServiceInitScript = "/etc/init.d/falcon-service"
 
 print("checkout repository")
 check_call(["git", "fetch", "--force"], cwd=sourcePath)
@@ -12,9 +14,18 @@ check_call(["git", "pull", "--force"], cwd=sourcePath)
 
 print("update scripts")
 copyfile(sourcePath + "/MilleniumFalconClient/falcon-update.py", falconUpdate)
+copyfile(sourcePath + "/MilleniumFalconClient/falcon-service.py", falconService)
+copyfile(sourcePath + "/MilleniumFalconClient/falcon-service.py", falconServiceInitScript)
 
 print("set owner of scripts")
 call(["chown", "root:root", falconUpdate])
+call(["chown", "root:root", falconService])
+call(["chown", "root:root", falconServiceInitScript])
 
 print("make scripts executable")
 call(["chmod", "755", falconUpdate])
+call(["chmod", "755", falconService])
+call(["chmod", "755", falconServiceInitScript])
+
+print("create pid files if necessary")
+call(["touch", "/var/run/falcon-service.pid"])
